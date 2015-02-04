899b15d3093b88d878df835f6943d2010ccb62b8
Peter Reuterås <peter@reuteras.com>
2015-02-04 06:37:42 +0100

    Added @namespace.

--- rtkeyboardshortcuts.meta.js
+++ rtkeyboardshortcuts.meta.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name            RT Keyboard Shortcuts
 // @description     Adds keyboard shortcuts to RT.
+// @namespace       https://reuteras.com/RT
 // @include         https://ticket.sys.kth.se/*
 // @include         https://rt.reuteras.com/*
 // @include         https://rt.cpan.org/*
 // @grant           none
 // @downloadURL     https://github.com/reuteras/RT-keyboard-shortcuts/raw/master/rtkeyboardshortcuts.user.js
 // @updateURL       https://github.com/reuteras/RT-keyboard-shortcuts/raw/master/rtkeyboardshortcuts.meta.js
-// @version         0.1.7
+// @version         0.1.8
 // ==/UserScript==
--- rtkeyboardshortcuts.user.js
+++ rtkeyboardshortcuts.user.js
@@ -56,13 +56,14 @@
 // ==UserScript==
 // @name            RT Keyboard Shortcuts
 // @description     Adds keyboard shortcuts to RT.
+// @namespace       https://reuteras.com/RT
 // @include         https://ticket.sys.kth.se/*
 // @include         https://rt.reuteras.com/*
 // @include         https://rt.cpan.org/*
 // @grant           none
 // @downloadURL     https://github.com/reuteras/RT-keyboard-shortcuts/raw/master/rtkeyboardshortcuts.user.js
 // @updateURL       https://github.com/reuteras/RT-keyboard-shortcuts/raw/master/rtkeyboardshortcuts.meta.js
-// @version         0.1.7
+// @version         0.1.8
 // ==/UserScript==
 // CHANGEME
 
@@ -74,7 +75,7 @@
  */
 function ShortcutsSource() {
     "use strict";
-    var myVersion = "Version of RT keyboard shourtcuts is 0.1.7", shortcutListener = {
+    var myVersion = "Version of RT keyboard shourtcuts is 0.1.8", shortcutListener = {
         listen: true,
         shortcut: null,
         combination: '',
